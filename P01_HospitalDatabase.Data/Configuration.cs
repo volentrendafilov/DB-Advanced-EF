4c96b1bda629760c0847343270151a99efe2685c
volentrendafilov <volentrendafilov@gmail.com>
2018-07-14 11:06:08 +0100

    Importing

---
+++ P01_HospitalDatabase.Data/Configuration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_HospitalDatabase.Data
+{
+    public class Configuration
+    {
+        public const string ConnectionString = @"Server=W5156132\SQLEXPRESS;Database=Hospital;Integrated Security=True";
+    }
+}
---
+++ P01_HospitalDatabase/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using P01_HospitalDatabase.Data;
+using P01_HospitalDatabase.Initializer;
+
+namespace P01_HospitalDatabase
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            using (HospitalContext context = new HospitalContext())
+            {
+                DatabaseInitializer.InitialSeed(context);
+            }
+        }
+    }
+}
